f2abfc30164d4364027fb2cdf55d86fa3dead698
MarcoProstamo <Marcopros1201@gmail.com>
2025-01-10 15:16:22 +0100

    Add → Movie Show

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,15 +2,37 @@ import { connection } from "../connections/moviesConnection.js";
 
 const controller = {
   index(req, res) {
-    const indexSql = `SELECT * FROM movies.movies;`;
+    const indexSql = `SELECT id, title, genre FROM movies.movies;`;
     connection.query(indexSql, (err, data) => {
-      if (err) res.json({ status: "KO", message: err.sqlMessage });
+      if (err) res.status(500).json({ status: "KO", message: err.sqlMessage });
       res.json(data);
     });
   },
 
   show(req, res) {
-    res.send(`Showing movie with ID ${req.params.id}`);
+    const id = parseInt(req.params.id);
+    if (isNaN(id))
+      res.status(400).json({ status: "KO", messagge: "Invalid Parameter" });
+
+    const showSql = `
+    SELECT movies.id AS movie_id, 
+       movies.title, 
+       movies.director, 
+       movies.genre, 
+       movies.release_year, 
+       movies.abstract, 
+       movies.image, 
+       GROUP_CONCAT(reviews.text SEPARATOR ' ') AS reviews
+    FROM movies.movies 
+    INNER JOIN reviews
+    ON movies.id = reviews.movie_id
+    WHERE movies.id = 1
+    GROUP BY movies.id;
+    `;
+    connection.query(showSql, [id], (err, data) => {
+      if (err) res.status(500).json({ status: "KO", message: err.sqlMessage });
+      res.json(data);
+    });
   },
 
   create(req, res) {
